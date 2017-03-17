269e93008b417cf583a98050560d578da70298b4
LoSt <zhengyuhang47@qq.com>
2017-03-17 17:04:36 +0800

    增加一些参数和使用示例

--- msgShow/js/index.js
+++ msgShow/js/index.js
@@ -1,8 +1,12 @@
 //  弹窗组件
-;(function ($) {
+;(function () {
     MsgBox = function(ops) {
         //  配置默认参数
         this.ops = {
+            width: null,
+            loading: false,
+            icon: null,
+            iconCls: null,
             msg: null,
             btns: null,
             delay: null,
@@ -20,7 +24,7 @@
         this.body = $('body');
         this.msgbox = $('<div class="popup msgbox">');
         this.container = $('<div class="msgbox-container">');
-        this.loading = $('<div class="msgbox-container-loading">');
+        this.icon = $('<div class="msgbox-container-icon iconfont">');
         this.tips = $('<h2 class="msgbox-container-tips">');
         this.btnsBox = $('<div class="msgbox-container-btns">');
 
@@ -41,7 +45,7 @@
                 body = this.body,
                 msgbox = this.msgbox,
                 container = this.container,
-                loading = this.loading,
+                icon = this.icon,
                 tips = this.tips,
                 btnsBox = this.btnsBox;
 
@@ -50,10 +54,30 @@
             msgbox.css("zIndex", MsgBox.zIndex);
 
             if (this.isOps) {
-                container.append(loading.text("loading.."));
+                container.append(icon.html("&#xe86b;").addClass("animate"));
                 msgbox.append(container.css("width", "auto"));
                 body.append(msgbox);
             } else {
+                //  弹窗宽度
+                if (ops.width) {
+                    container.css("width", ops.width);
+                }
+
+                //  是否执行loading动画
+                if (ops.loading) {
+                    icon.addClass("animate");
+                }
+
+                //  图标状态
+                if (ops.icon) {
+                    container.append(icon.html(ops.icon));
+                }
+
+                //  图标增加class
+                if (ops.iconCls) {
+                    icon.addClass(ops.iconCls);
+                }
+
                 //  文字信息
                 if (ops.msg) {
                     container.append(tips.text(ops.msg));
@@ -72,7 +96,7 @@
 
                         //  如果有回调函数则执行
                         ops.delayCallback && ops.delayCallback();
-                    }, ops.delay)
+                    }, ops.delay * 1000)
                 }
 
                 msgbox.append(container);
@@ -95,6 +119,7 @@
                 btn.tap(function () {
                     var isClose = callback && callback();
 
+                    //  绑定事件不返回 false ，则默认关闭该弹窗
                     if (isClose != false) {
                         _this.close();
                     }
@@ -114,4 +139,4 @@
 
     //  注册MsgBox到window对象上
     window.MsgBox = MsgBox;
-})(Zepto);
+})();
