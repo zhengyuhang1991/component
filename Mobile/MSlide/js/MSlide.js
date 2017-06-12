bf1ef0fba6f4367f57613950a4d11a36cec168d4
LoSt <zhengyuhang47@qq.com>
2017-06-12 23:10:58 +0800

    MSlide完成循环滑动,自动展示;

--- Mobile/MSlide/js/MSlide.js
+++ Mobile/MSlide/js/MSlide.js
@@ -11,8 +11,7 @@
         //  扩展默认参数
         $.extend(this.ops, ops);
 
-        var el = this.ops.el,
-            _this = this;
+        var el = this.ops.el;
 
         //  获取目标DOM
         this.MSlide = $(el);
@@ -29,14 +28,13 @@
         init: function () {
             var ops = this.ops;
 
-            //  根据扩展参数来执行对应方法
+            //  创建索引块
             if (ops.createIdx) {
                 this.createIdx();
             }
 
-            /*if (ops.autoPlay) {
-                this.autoPlay();
-            }*/
+            //  创建循环展示DOM
+            this.createLoopDom();
 
             //  开始滑动
             this.start();
@@ -55,11 +53,14 @@
                 timer = null,   //  定时器
                 autoPlayTime = ops.autoPlayTime * 1000;   //  切换间隔时间
 
+            this.setValue(idx);
 
             //开始滑动事件
             var startEvent = function(ev) {
                 _this.startX = _this.backPlace(ev); //触摸坐标
                 _this.offsetX = 0;                  //滑动距离
+
+                clearInterval(timer);
             }
             //正在滑动事件
             var moveEvent = function (ev) {
@@ -75,25 +76,57 @@
             var endEvent = function () {
                 var border = width / 5;
 
+                _this.setTransition();
+
                 if (_this.offsetX > border) {
                     idx--;
+
+                    if (idx < 0) {
+                        idx = len - 2;
+                        _this.setValue(idx, _this.offsetX);
+
+                        idx--;
+                    }
                 } else if (_this.offsetX < -border) {
                     idx++;
+
+                    if (idx >= len - 2) {
+                        idx = -1;
+                        _this.setValue(idx, _this.offsetX);
+
+                        idx++;
+                    }
                 }
 
-                if (idx < 0) {
-                    idx = 0;
-                } else if (idx > len - 1) {
-                    idx = len - 1;
-                }
-
-                _this.setTransition(.3);      //滑动结束时添加过渡时间
                 _this.setValue(idx);
+                _this.setTransition(.3);      //滑动结束时添加过渡时间
+
+                timer = setInterval(autoPlay, autoPlayTime);
             }
 
             el.on("touchstart", startEvent)
                 .on("touchmove", moveEvent)
                 .on("touchend", endEvent);
+
+            timer = setInterval(autoPlay, autoPlayTime);
+
+            /*
+             * 自动轮播展示
+             * */
+            function autoPlay() {
+                if (idx >= len - 3) {
+                    idx = -1;
+                    _this.setTransition();
+                    _this.setValue(idx);
+
+                    idx++;
+                } else {
+                    idx++;
+                }
+
+                _this.setValue(idx);
+                _this.setTransition(.3);        //添加过渡时间
+            }
         },
         /*
          * 生成索引块
@@ -145,6 +178,8 @@
                 list = this.list,
                 width = list.width();
 
+            idx++;
+
             if (val) {
                 list.css({
                     transform: "translate3d(" + ((-idx * width) + val) + "px, 0, 0)",
@@ -159,9 +194,21 @@
 
             if (ops.createIdx) {
                 MSlide.find(".MSlide-idx li").removeClass("on")
-                    .eq(idx).addClass("on");
+                    .eq(idx - 1).addClass("on");
             }
         },
+        /*
+        * 生成无缝展示DOM
+        * */
+        createLoopDom: function () {
+            var list = this.list,
+                ul = list.children("ul"),
+                firstLi = ul.find("li:first-child").clone(),
+                lastLi = ul.find("li:last-child").clone();
+
+            ul.append(firstLi)
+                .prepend(lastLi);
+        }
     };
 
     //  注册MSlide到window对象上
